d5e96870f5d56bce9e5614e650675c17a17c812d
Trade With Qadeer <103352762+AbdulMods@users.noreply.github.com>
2025-06-25 15:08:48 +0500

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -111,8 +111,8 @@ install_package() {
         return 1
     fi
 }
-#custimization 
 
+# Customization 
 customize_terminal() {
     if [[ "$OS" == "termux" ]]; then
         step "Customizing Termux terminal" "🎨"
@@ -121,22 +121,22 @@ customize_terminal() {
         cat > ~/../usr/etc/bash.bashrc << 'EOL'
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Custom Trade With Qadeer Terminal
+# Custom Trade Terminal
 echo -e "\033[1;91m
-  __    __   ____  ____  ____  ____ 
- /  \  / _\ (    \(  __)(  __)(  _ \
-(  O )/    \ ) D ( ) _)  ) _)  )   /
- \__\)\_/\_/(____/(____)(____)(__\_)
+  ____  ____  ____  ____ 
+ /  _ \/  _ \/  _ \/  _ \
+(  (_) )  (_) )  (_) )  (_) )
+ \____/\____/\____/\____/
 \033[0m"
 
-echo -e "\033[1;92m❤️  Welcome to ForexContactsPro Terminal!\033[0m"
+echo -e "\033[1;92m❤️  Welcome to Trade Terminal!\033[0m"
 echo -e "\033[1;93m📢  Educational Use Only - Trade Responsibly\033[0m"
-echo -e "\033[1;94m© $(date +%Y) Trade With Qadeer. All rights reserved.\033[0m"
+echo -e "\033[1;94m© $(date +%Y) Trade. All rights reserved.\033[0m"
 echo ""
 
 # Show installed tools
 echo -e "\033[1;96mInstalled Tools:\033[0m"
-echo -e "  \033[1;95m• ForexContactsPro\033[0m"
+echo -e "  \033[1;95m• Trade Terminal\033[0m"
 echo -e "    Run command: \033[1;97mqadeer\033[0m"
 echo ""
 
@@ -148,7 +148,7 @@ echo -e "  \033[1;95m• Time:\033[0m $(date +%T)"
 echo ""
 
 # Custom prompt with colors, time and directory
-PS1='\[\033[1;91m\]\t \[\033[1;92m\]➤ \[\033[1;96m\]\w \[\033[1;91m\]\$ \[\033[0m\]'
+PS1='\[\033[1;91m\]\t \[\033[1;92m\]Qadeer➤ \[\033[1;96m\]\w \[\033[1;91m\]\$ ___BLOCK_OPEN___\033[0m___BLOCK_CLOSE___'
 EOL
 
         # Create custom motd
@@ -162,7 +162,7 @@ ________________________________________________________
 |       ██║   ██║  ██║██║  ██║██████╔╝███████╗         |
 |       ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝         |
 |                                                      |
-| ForexContactsPro Tool by Abdul Qadeer Gabol          |
+| Trade Tool by Abdul Qadeer Gabol                     |
 | Educational Use Only - Trade Responsibly             |
 |______________________________________________________|
 EOL
@@ -187,27 +187,27 @@ EOL
             # Append customizations to bashrc
             cat >> ~/.bashrc << 'EOL'
 
-# Custom Trade With Qadeer Terminal Configuration
+# Custom Trade Terminal Configuration
 echo -e "\033[1;91m
-  __    __   ____  ____  ____  ____ 
- /  \  / _\ (    \(  __)(  __)(  _ \
-(  O )/    \ ) D ( ) _)  ) _)  )   /
- \__\)\_/\_/(____/(____)(____)(__\_)
+  ____  ____  ____  ____ 
+ /  _ \/  _ \/  _ \/  _ \
+(  (_) )  (_) )  (_) )  (_) )
+ \____/\____/\____/\____/
 \033[0m"
 
-echo -e "\033[1;92m❤️  Welcome to ForexContactsPro Terminal!\033[0m"
+echo -e "\033[1;92m❤️  Welcome to Trade Terminal!\033[0m"
 echo -e "\033[1;93m📢  Educational Use Only - Trade Responsibly\033[0m"
-echo -e "\033[1;94m© $(date +%Y) Trade With Qadeer. All rights reserved.\033[0m"
+echo -e "\033[1;94m© $(date +%Y) Trade. All rights reserved.\033[0m"
 echo ""
 
 # Show installed tools
 echo -e "\033[1;96mInstalled Tools:\033[0m"
-echo -e "  \033[1;95m• ForexContactsPro\033[0m"
+echo -e "  \033[1;95m• Trade Terminal\033[0m"
 echo -e "    Run command: \033[1;97mqadeer\033[0m"
 echo ""
 
 # Custom prompt with time and directory
-PS1='\[\033[1;91m\]\t \[\033[1;92m\]➤ \[\033[1;96m\]\w \[\033[1;91m\]\$ \[\033[0m\]'
+PS1='___BLOCK_OPEN___\033[1;91m___BLOCK_CLOSE___\t ___BLOCK_OPEN___\033[1;92m___BLOCK_CLOSE___Qadeer➤ ___BLOCK_OPEN___\033[1;96m___BLOCK_CLOSE___\w ___BLOCK_OPEN___\033[1;91m___BLOCK_CLOSE___\$ ___BLOCK_OPEN___\033[0m___BLOCK_CLOSE___'
 
 # Custom ls colors
 LS_COLORS='rs=0:di=01;94:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:'
@@ -221,7 +221,6 @@ EOL
         fi
     fi
 }
-
 # Installation process
 install_all() {
     if [[ "$OS" == "termux" ]]; then
@@ -249,11 +248,7 @@ install_all() {
     install_package git || return 1
 
     step "Installing Figlet" "✨"
-    if [[ "$OS" == "termux" ]]; then
-        install_package figlet
-    else
-        install_package figlet
-    fi
+    install_package figlet
 
     # Clean existing installation
     step "Checking for previous installations" "🧹"
